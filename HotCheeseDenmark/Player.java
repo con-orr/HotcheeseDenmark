82e108c5b177994861a7072858157b4ef0187b95
BraydenPayne <bp4hg06@limestone.on.ca>
2023-06-08 09:16:10 -0400

    movement keys added
    WASD keys, along with boolean checking for hits

--- HotCheeseDenmark/Player.java
+++ HotCheeseDenmark/Player.java
@@ -8,12 +8,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Actor
 {
+    int deltaX = 0; // x velocity
+    int deltaY = 0; // y velocity
+    boolean isHit = false; // checks if player is damaged
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        // temporary movement keys
+        if (Greenfoot.isKeyDown("w")) {
+            deltaY = 3;
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            deltaY = -3;
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            deltaX = -3;
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            deltaX = 3;
+        }
+        setLocation(getX() + deltaX, getY() + deltaY);
     }
 }
