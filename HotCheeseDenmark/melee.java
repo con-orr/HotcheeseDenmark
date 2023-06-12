a7e0868614a195a699bf4344757fa5fd08804859
BraydenPayne <bp4hg06@limestone.on.ca>
2023-06-12 09:15:55 -0400

    melee actor updated
    fist png placed in world on input

--- HotCheeseDenmark/melee.java
+++ HotCheeseDenmark/melee.java
@@ -15,5 +15,8 @@ public class melee extends Actor
     public void act()
     {
         // Add your action code here.
+        if (Greenfoot.isKeyDown("space")) {
+            
+        }
     }
 }
