b38cd0e515326480d8e39a140ced7974d8392166
DylanH1 <125473612+DylanH1@users.noreply.github.com>
2023-06-13 09:07:43 -0400

    Added new sprites

---
+++ HotCheeseDenmark/PlayerBat.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PlayerBat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PlayerBat extends Actor
+{
+    /**
+     * Act - do whatever the PlayerBat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
