1e88943dd737ca4ef1249d8b8582408f46f2a3ff
con-orr <jackco@limestone.on.ca>
2023-06-14 08:21:00 -0400

    dwadsawdsa

--- HotCheeseDenmark/Player.java
+++ HotCheeseDenmark/Player.java
@@ -12,7 +12,8 @@ public class Player extends Actor
     int deltaX = 0; // x velocity
     int deltaY = 0; // y velocity
     boolean isHit = false; // checks if player is damaged
-    Actor actorGrabbed = null;
+    boolean HoldingItem = false;
+    Weapon2 currentItem;
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -20,24 +21,7 @@ public class Player extends Actor
     public void act()
     {
         //item pickup and dropping
-        if (Greenfoot.isKeyDown("g") && actorGrabbed == null && getOneIntersectingObject(Weapon2.class) != null) {
-            // grab the object
-            actorGrabbed = (Actor) getOneIntersectingObject(Weapon2.class);
-            // the rest of this block will avoid the grabbed object from being hidden UNDER the Actor1 objects
-            getWorld().removeObject(actorGrabbed);
-            getWorld().addObject(actorGrabbed, getX(), getY());
-        }
-        // check for actual dragging of the object after moving object
-        if (Greenfoot.isKeyDown("g") && actorGrabbed != null)
-        {
-            // drag the grabbed object
-            actorGrabbed.setLocation(getX(), getY());
-        }
-        if (Greenfoot.isKeyDown("q") && actorGrabbed != null)
-        {
-            // release the object
-            actorGrabbed = null;
-        }
+        
         //movement keys
 
         if (Greenfoot.isKeyDown("w")) {
@@ -71,6 +55,9 @@ public class Player extends Actor
                 getWorld().addObject(new melee(), getX() + 30, getY());
             }
         }
+    }
+    private void PickUpItem(){
+        Item Weapon2 = 
     }
 
 }
