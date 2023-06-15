5a027183fec4e49c51e046f09091135c21c1c0bd
Landon2R <125473648+Landon2R@users.noreply.github.com>
2023-06-15 11:00:59 -0400

    In the process of adding pathfinding this is update 1

--- HotCheeseDenmark/Enemy.java
+++ HotCheeseDenmark/Enemy.java
@@ -18,6 +18,8 @@ public class Enemy extends Actor
     String weapon = "fists";
     int speed = 4;
     int delay = 60;
+    int[] playerCord = new int[2];
+    int[] enemyCord = new int[2];
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -118,39 +120,133 @@ public class Enemy extends Actor
         }
         delay = 60;
     }
+
     public int[][] getMap(){
         int[][] map = new int[getWorld().getHeight()][getWorld().getWidth()];
         for(int i = 0; i<getWorld().getHeight(); i++){
             for(int j = 0; j<getWorld().getWidth(); j++){
                 if(MyWorld.p1.getY()==i && MyWorld.p1.getX()==j){
                     map[i][j] = 1;
+                    playerCord[0]=j;
+                    playerCord[1]=i;
                 }
                 if(getWorld().getColorAt(j,i).equals(wall)){
                     map[i][j]=2;
                 }
                 if(getY()==i && getX() == j){
                     map[i][j] = 3;
+                    enemyCord[0]=j;
+                    enemyCord[1]=i;
                 }
             }
         }
         return map;
     }
+
     public void pathfind(){
+        // IMPORTANT**** do not question the effeciency or effectivness of this section it's totally fine. 
+        //I will attempt to add coments to explain but honestly I'm not sure how much they'll help.
         int[][] map = getMap();
-        int[] playerCord = new int[2];
-        int[] enemyCord = new int[2];
-        for(int i = 0; i<getWorld().getHeight(); i++){
-            for(int j = 0; j<getWorld().getWidth(); j++){
-                if(map[i][j]==3){
-                    enemyCord[0]=j;
-                    enemyCord[1]=i;
+        int pixlesX = 0;
+        int pixlesY = 0;
+        for(int i = 0; i<map.length;){
+            for(int j = 0; j<map[i].length;){
+                if(playerCord[0]>enemyCord[0]&&map[i][j+1]!=2){
+                    j++;
+                    pixlesX++;
                 }
-                if(map[i][j]==1){
-                    playerCord[0]=j;
-                    playerCord[1]=i;
+                else if(playerCord[0]<enemyCord[0]&&map[i][j+1]==2){
+                    if(playerCord[1]>enemyCord[1]){
+                        for(int q = 0; q<getWorld().getHeight()-enemyCord[1];q++){
+                            if(map[i+q][j-1]!=2){
+                                i+=q;
+                                pixlesY+=q;
+                            }
+                        }
+                    }
+                    else if(playerCord[1]<enemyCord[1]){
+                        for(int q = 0; q<0+enemyCord[1];q--){
+                            if(map[i+q][j+1]!=2){
+                                i-=q;
+                                pixlesY-=q;
+                            }
+                        }
+                    }
+                }
+                if(playerCord[0]<enemyCord[0]&&map[i][j-1]!=2){
+                    j--;
+                    pixlesX--;
+                }
+                else if(playerCord[0]<enemyCord[0]&&map[i][j-1]==2){
+                    if(playerCord[1]>enemyCord[1]){
+                        for(int q = 0; q<getWorld().getHeight()-enemyCord[1];q++){
+                            if(map[i+q][j-1]!=2){
+                                i+=q;
+                                pixlesY+=q;
+                            }
+                        }
+                    }
+                    else if(playerCord[1]<enemyCord[1]){
+                        for(int q = 0; q<0+enemyCord[1];q--){
+                            if(map[i+q][j+1]!=2){
+                                i-=q;
+                                pixlesY-=q;
+                            }
+                        }
+                    }
+                }
+                if(playerCord[1]<enemyCord[1]&&map[i-1][j]!=2){
+                    i--;
+                    pixlesY--;
+                }
+                else if(playerCord[1]<enemyCord[1]&&map[i-1][j]==2){
+                    if(playerCord[0]>enemyCord[0]){
+                        for(int q = 0; q<getWorld().getWidth()-enemyCord[0];q++){
+                            if(map[i+1][j+q]!=2){
+                                j+=q;
+                                pixlesX+=q;
+                            }
+                        }
+                    }
+                    else if(playerCord[0]<enemyCord[0]){
+                        for(int q = 0; q<0+enemyCord[0];q--){
+                            if(map[i+1][j+q]!=2){
+                                j-=q;
+                                pixlesX-=q;
+                            }
+                        }
+                    }
+                }
+                if(playerCord[1]>enemyCord[1]&&map[i+1][j]!=2){
+                    i++;
+                    pixlesY++;
+                }
+                else if(playerCord[1]>enemyCord[1]&&map[i+1][j]==2){
+                    if(playerCord[0]>enemyCord[0]){
+                        for(int q = 0; q<getWorld().getWidth()-enemyCord[0];q++){
+                            if(map[i+1][j+q]!=2){
+                                j+=q;
+                                pixlesX+=q;
+                            }
+                        }
+                    }
+                    else if(playerCord[0]<enemyCord[0]){
+                        for(int q = 0; q<0+enemyCord[0];q--){
+                            if(map[i+1][j+q]!=2){
+                                j-=q;
+                                pixlesX-=q;
+                            }
+                        }
+                    }
+                }
+                
+                if(j==playerCord[0]){
+                    break;
                 }
             }
+            if(i==playerCord[1]){
+                break;
+            }
         }
-        
     }
 }
