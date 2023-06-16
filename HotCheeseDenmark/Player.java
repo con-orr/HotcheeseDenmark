a23d2e4afa07acc3e82bb86f45f7e09a9eb767d8
BraydenPayne <bp4hg06@limestone.on.ca>
2023-06-16 08:29:36 -0400

    added wall detection
    cannot walk through walls now

--- HotCheeseDenmark/Player.java
+++ HotCheeseDenmark/Player.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.Color; // colors and stuff
 /**
  * Write a description of class Player here.
  * 
@@ -13,29 +13,36 @@ public class Player extends Actor
     int deltaY = 0; // y velocity
     boolean isHit = false; // checks if player is damaged\
     int cooldown = 0; // cooldown for attack
+    boolean[] playerDirection = {true, true, true, true};
+    //                           right left  down   up
+    Color wall = new Color(0, 0, 0); // Color of wall (black)
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        playerDirectionDetection();
         cooldown++;
         // movement keys
-        if (Greenfoot.isKeyDown("w")) {
-            move(-3);
+        if (Greenfoot.isKeyDown("w") && playerDirection[3]) {
+            setLocation(getX(), getY() - 3);
             direction = 'w';
         }
-        if (Greenfoot.isKeyDown("s")) {
-            move(3);
+        if (Greenfoot.isKeyDown("s") && playerDirection[2]) {
+            setLocation(getX(), getY() + 3);
             direction = 's';
         }
-        if (Greenfoot.isKeyDown("a")) {
+        if (Greenfoot.isKeyDown("a") && playerDirection[1]) {
             setLocation(getX() - 3, getY());
             direction = 'a';
         }
-        if (Greenfoot.isKeyDown("d")) {
+        if (Greenfoot.isKeyDown("d") && playerDirection[0]) {
             setLocation(getX() + 3, getY());
             direction = 'd';
+        }
+        if (getWorld().getColorAt(getX(), getY()).equals(wall)) {
+            
         }
         // melee attack
         if (Greenfoot.isKeyDown("space") && cooldown > 30) {
@@ -63,6 +70,34 @@ public class Player extends Actor
         }
         if (Greenfoot.isKeyDown("right")) {
             turn(5);
+        }
+    }
+    public void playerDirectionDetection(){
+        if(getX()+3<getWorld().getWidth()&& getX()-3>0 && getY()+3<getWorld().getHeight() && getY()-3>0){
+            if(getWorld().getColorAt(getX()+3,getY()).equals(wall)){
+                playerDirection[0]=false;
+            }
+            else{
+                playerDirection[0]=true;
+            }
+            if(getWorld().getColorAt(getX()-3,getY()).equals(wall)){
+                playerDirection[1]=false;
+            }
+            else{
+                playerDirection[1]=true;
+            }
+            if(getWorld().getColorAt(getX(),getY()+3).equals(wall)){
+                playerDirection[2]=false;
+            }
+            else{
+                playerDirection[2]=true;
+            }
+            if(getWorld().getColorAt(getX(),getY()-3).equals(wall)){
+                playerDirection[3]=false;
+            }
+            else{
+                playerDirection[3]=true;
+            }
         }
     }
 }
